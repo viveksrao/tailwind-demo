e14001e410d66dbee0d905f00f287133f61d0001
Vivekanand Rao <vrao@egnyte.com>
2020-01-19 19:23:13 -0800

    Structure a Basic Card in Tailwind CSS

--- src/App.vue
+++ src/App.vue
@@ -60,15 +60,20 @@
 				</div>
 			</div>
 		</div>
+		<div class="bg-gray-200 min-h-screen p-8 flex items-center justify-center">
+			<PropertyCard :property="property" />
+		</div>
 	</div>
 </template>
 
 <script>
 import DestinationCard from "./components/DestinationCard";
+import PropertyCard from "./components/PropertyCard";
 export default {
 	name: "app",
 	components: {
-		DestinationCard
+		DestinationCard,
+		PropertyCard
 	},
 	data() {
 		return {
@@ -115,7 +120,19 @@ export default {
 					imageUrl: "/img/miami.jpg",
 					imageAlt: "Beach in Miami"
 				}
-			]
+			],
+			property: {
+				imageUrl:
+					"https://images.unsplash.com/photo-1512917774080-9991f1c4c750?ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=80",
+				imageAlt: "Rear view of modern home with pool",
+				beds: 3,
+				baths: 2,
+				title: "Modern home in city center",
+				priceInCents: 190000,
+				formattedPrice: "$1,900.00",
+				reviewCount: 34,
+				rating: 4
+			}
 		};
 	}
 };
